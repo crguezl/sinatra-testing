4857c191d8c9006fe18e51048985ae1957797cb7
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2014-10-02 15:45:39 +0100

    example of inline template with layout

--- inline_template_with_layout.rb
+++ inline_template_with_layout.rb
@@ -1,14 +1,27 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
+# If a template named 'layout' exists, it will be used each time a
+# template is rendered. You can individually disable layouts by passing
+#             :layout => false 
+# or disable them by default via 
+#             set :haml, :layout => false
+
+set :haml, :layout => false if ARGV.length > 0
+
 get '/' do
   haml :index
+end
+
+get '/nolayout' do
+  haml :index, :layout =>false
 end
 
 __END__
 
 @@ layout
 %html
+  <h1>A Simple Layout</h1>
   = yield
 
 @@ index
